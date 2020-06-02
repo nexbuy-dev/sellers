482ffd21708e09de46493bbe195692e339ba4d33
oluwavicky1 <iwatannayevictor@gmail.com>
2020-06-02 02:42:10 +0100

    Updates

---
+++ precache-manifest.5efbb80900c54ea3fb8867875f4e25a2.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0089ce57f018c6f97fc1a62b0aaab790",
+    "url": "/sellers/index.html"
+  },
+  {
+    "revision": "a52039d9c6dd991b6272",
+    "url": "/sellers/static/css/2.1dfcba2e.chunk.css"
+  },
+  {
+    "revision": "59024581d047a261bc90",
+    "url": "/sellers/static/css/main.d3c0d1fa.chunk.css"
+  },
+  {
+    "revision": "a52039d9c6dd991b6272",
+    "url": "/sellers/static/js/2.a0b28f5d.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/sellers/static/js/2.a0b28f5d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59024581d047a261bc90",
+    "url": "/sellers/static/js/main.fe086c06.chunk.js"
+  },
+  {
+    "revision": "feeb5cf52730dc071e11",
+    "url": "/sellers/static/js/runtime-main.9d26b472.js"
+  },
+  {
+    "revision": "d495a7622908378219780a9201eb43e2",
+    "url": "/sellers/static/media/adidas.d495a762.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/sellers/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/sellers/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/sellers/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/sellers/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/sellers/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c9a97ca67d4620e05c66a487b8fa088e",
+    "url": "/sellers/static/media/girl_street_shopping_cape_town_38679_1920x1080.c9a97ca6.jpg"
+  },
+  {
+    "revision": "e834f5f2763a419e20e6c8c0d3280281",
+    "url": "/sellers/static/media/happy-couple-man-woman-with-shopping-bags-isolated_97712-1265.e834f5f2.jpg"
+  },
+  {
+    "revision": "dd3ee982fd40640da38c26c5d618bd01",
+    "url": "/sellers/static/media/new.dd3ee982.png"
+  },
+  {
+    "revision": "808dc2629e20ae19a31aa3ad25c9705d",
+    "url": "/sellers/static/media/nexbuy.808dc262.svg"
+  },
+  {
+    "revision": "df3e441469691db98426c5b558c9a7ff",
+    "url": "/sellers/static/media/nike.df3e4414.jpg"
+  },
+  {
+    "revision": "19e99f2501bb6222fe854b386cb9e6bd",
+    "url": "/sellers/static/media/oluwavicky.19e99f25.jpg"
+  },
+  {
+    "revision": "09667668c5f68027f26d785861e32015",
+    "url": "/sellers/static/media/shopping-smile.09667668.jpg"
+  },
+  {
+    "revision": "c689d76a3503474e925abb290b225322",
+    "url": "/sellers/static/media/suit1.c689d76a.jpg"
+  }
+]);
